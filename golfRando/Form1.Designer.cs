22e0cf7797997dea021743720b24192ea097dcef
Patrick Fifield <plfifield9@yahoo.com>
2018-02-10 08:37:13 -0500

    Added a bunch of randomizations - Added hole location, tee box, obstacle, and club randomization

--- golfRando/Form1.Designer.cs
+++ golfRando/Form1.Designer.cs
@@ -28,12 +28,290 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblFlags = new System.Windows.Forms.Label();
+            this.txtFlags = new System.Windows.Forms.TextBox();
+            this.btnNewSeed = new System.Windows.Forms.Button();
+            this.lblSeed = new System.Windows.Forms.Label();
+            this.txtSeed = new System.Windows.Forms.TextBox();
+            this.btnCompareBrowse = new System.Windows.Forms.Button();
+            this.lblCompareImage = new System.Windows.Forms.Label();
+            this.txtCompare = new System.Windows.Forms.TextBox();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.lblReqChecksum = new System.Windows.Forms.Label();
+            this.lblRequired = new System.Windows.Forms.Label();
+            this.lblSHAChecksum = new System.Windows.Forms.Label();
+            this.lblSHA = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblRomImage = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.chkHoleLocations = new System.Windows.Forms.CheckBox();
+            this.chkObstacles = new System.Windows.Forms.CheckBox();
+            this.chkTeeBoxes = new System.Windows.Forms.CheckBox();
+            this.chkClubs = new System.Windows.Forms.CheckBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.cmdRandomize = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFlags
+            // 
+            this.lblFlags.AutoSize = true;
+            this.lblFlags.Location = new System.Drawing.Point(291, 113);
+            this.lblFlags.Name = "lblFlags";
+            this.lblFlags.Size = new System.Drawing.Size(32, 13);
+            this.lblFlags.TabIndex = 122;
+            this.lblFlags.Text = "Flags";
+            // 
+            // txtFlags
+            // 
+            this.txtFlags.Location = new System.Drawing.Point(329, 109);
+            this.txtFlags.Name = "txtFlags";
+            this.txtFlags.Size = new System.Drawing.Size(200, 20);
+            this.txtFlags.TabIndex = 121;
+            // 
+            // btnNewSeed
+            // 
+            this.btnNewSeed.Location = new System.Drawing.Point(186, 109);
+            this.btnNewSeed.Name = "btnNewSeed";
+            this.btnNewSeed.Size = new System.Drawing.Size(86, 23);
+            this.btnNewSeed.TabIndex = 113;
+            this.btnNewSeed.Text = "New Seed";
+            this.btnNewSeed.UseVisualStyleBackColor = true;
+            this.btnNewSeed.Click += new System.EventHandler(this.btnNewSeed_Click);
+            // 
+            // lblSeed
+            // 
+            this.lblSeed.AutoSize = true;
+            this.lblSeed.Location = new System.Drawing.Point(12, 113);
+            this.lblSeed.Name = "lblSeed";
+            this.lblSeed.Size = new System.Drawing.Size(32, 13);
+            this.lblSeed.TabIndex = 120;
+            this.lblSeed.Text = "Seed";
+            // 
+            // txtSeed
+            // 
+            this.txtSeed.Location = new System.Drawing.Point(69, 111);
+            this.txtSeed.Name = "txtSeed";
+            this.txtSeed.Size = new System.Drawing.Size(100, 20);
+            this.txtSeed.TabIndex = 112;
+            // 
+            // btnCompareBrowse
+            // 
+            this.btnCompareBrowse.Location = new System.Drawing.Point(458, 33);
+            this.btnCompareBrowse.Name = "btnCompareBrowse";
+            this.btnCompareBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnCompareBrowse.TabIndex = 110;
+            this.btnCompareBrowse.Text = "Browse";
+            this.btnCompareBrowse.UseVisualStyleBackColor = true;
+            // 
+            // lblCompareImage
+            // 
+            this.lblCompareImage.AutoSize = true;
+            this.lblCompareImage.Location = new System.Drawing.Point(12, 35);
+            this.lblCompareImage.Name = "lblCompareImage";
+            this.lblCompareImage.Size = new System.Drawing.Size(94, 13);
+            this.lblCompareImage.TabIndex = 119;
+            this.lblCompareImage.Text = "Comparison Image";
+            // 
+            // txtCompare
+            // 
+            this.txtCompare.Location = new System.Drawing.Point(132, 35);
+            this.txtCompare.Name = "txtCompare";
+            this.txtCompare.Size = new System.Drawing.Size(320, 20);
+            this.txtCompare.TabIndex = 109;
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(458, 62);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(75, 23);
+            this.btnCompare.TabIndex = 111;
+            this.btnCompare.Text = "Compare";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            // 
+            // lblReqChecksum
+            // 
+            this.lblReqChecksum.AutoSize = true;
+            this.lblReqChecksum.Location = new System.Drawing.Point(129, 88);
+            this.lblReqChecksum.Name = "lblReqChecksum";
+            this.lblReqChecksum.Size = new System.Drawing.Size(241, 13);
+            this.lblReqChecksum.TabIndex = 118;
+            this.lblReqChecksum.Text = "8d2b8aea636a2239805c99744bf48c0b4df8d96e";
+            // 
+            // lblRequired
+            // 
+            this.lblRequired.AutoSize = true;
+            this.lblRequired.Location = new System.Drawing.Point(12, 88);
+            this.lblRequired.Name = "lblRequired";
+            this.lblRequired.Size = new System.Drawing.Size(50, 13);
+            this.lblRequired.TabIndex = 117;
+            this.lblRequired.Text = "Required";
+            // 
+            // lblSHAChecksum
+            // 
+            this.lblSHAChecksum.AutoSize = true;
+            this.lblSHAChecksum.Location = new System.Drawing.Point(129, 64);
+            this.lblSHAChecksum.Name = "lblSHAChecksum";
+            this.lblSHAChecksum.Size = new System.Drawing.Size(247, 13);
+            this.lblSHAChecksum.TabIndex = 116;
+            this.lblSHAChecksum.Text = "????????????????????????????????????????";
+            // 
+            // lblSHA
+            // 
+            this.lblSHA.AutoSize = true;
+            this.lblSHA.Location = new System.Drawing.Point(12, 64);
+            this.lblSHA.Name = "lblSHA";
+            this.lblSHA.Size = new System.Drawing.Size(88, 13);
+            this.lblSHA.TabIndex = 115;
+            this.lblSHA.Text = "SHA1 Checksum";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(458, 7);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 108;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lblRomImage
+            // 
+            this.lblRomImage.AutoSize = true;
+            this.lblRomImage.Location = new System.Drawing.Point(12, 9);
+            this.lblRomImage.Name = "lblRomImage";
+            this.lblRomImage.Size = new System.Drawing.Size(86, 13);
+            this.lblRomImage.TabIndex = 114;
+            this.lblRomImage.Text = "Golf ROM Image";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(132, 9);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(320, 20);
+            this.txtFileName.TabIndex = 107;
+            // 
+            // chkHoleLocations
+            // 
+            this.chkHoleLocations.AutoSize = true;
+            this.chkHoleLocations.Location = new System.Drawing.Point(15, 150);
+            this.chkHoleLocations.Name = "chkHoleLocations";
+            this.chkHoleLocations.Size = new System.Drawing.Size(153, 17);
+            this.chkHoleLocations.TabIndex = 123;
+            this.chkHoleLocations.Text = "Randomize Hole Locations";
+            this.chkHoleLocations.UseVisualStyleBackColor = true;
+            this.chkHoleLocations.CheckedChanged += new System.EventHandler(this.determineFlags);
+            // 
+            // chkObstacles
+            // 
+            this.chkObstacles.AutoSize = true;
+            this.chkObstacles.Location = new System.Drawing.Point(15, 173);
+            this.chkObstacles.Name = "chkObstacles";
+            this.chkObstacles.Size = new System.Drawing.Size(129, 17);
+            this.chkObstacles.TabIndex = 124;
+            this.chkObstacles.Text = "Randomize Obstacles";
+            this.chkObstacles.UseVisualStyleBackColor = true;
+            this.chkObstacles.CheckedChanged += new System.EventHandler(this.determineFlags);
+            // 
+            // chkTeeBoxes
+            // 
+            this.chkTeeBoxes.AutoSize = true;
+            this.chkTeeBoxes.Location = new System.Drawing.Point(15, 196);
+            this.chkTeeBoxes.Name = "chkTeeBoxes";
+            this.chkTeeBoxes.Size = new System.Drawing.Size(122, 17);
+            this.chkTeeBoxes.TabIndex = 125;
+            this.chkTeeBoxes.Text = "Randomize Tee Box";
+            this.chkTeeBoxes.UseVisualStyleBackColor = true;
+            this.chkTeeBoxes.CheckedChanged += new System.EventHandler(this.determineFlags);
+            // 
+            // chkClubs
+            // 
+            this.chkClubs.AutoSize = true;
+            this.chkClubs.Location = new System.Drawing.Point(15, 219);
+            this.chkClubs.Name = "chkClubs";
+            this.chkClubs.Size = new System.Drawing.Size(108, 17);
+            this.chkClubs.TabIndex = 126;
+            this.chkClubs.Text = "Randomize Clubs";
+            this.chkClubs.UseVisualStyleBackColor = true;
+            this.chkClubs.CheckedChanged += new System.EventHandler(this.determineFlags);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 309);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 130;
+            // 
+            // cmdRandomize
+            // 
+            this.cmdRandomize.Location = new System.Drawing.Point(437, 248);
+            this.cmdRandomize.Name = "cmdRandomize";
+            this.cmdRandomize.Size = new System.Drawing.Size(96, 23);
+            this.cmdRandomize.TabIndex = 131;
+            this.cmdRandomize.Text = "Randomize";
+            this.cmdRandomize.UseVisualStyleBackColor = true;
+            this.cmdRandomize.Click += new System.EventHandler(this.cmdRandomize_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(555, 359);
+            this.Controls.Add(this.cmdRandomize);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.chkClubs);
+            this.Controls.Add(this.chkTeeBoxes);
+            this.Controls.Add(this.chkObstacles);
+            this.Controls.Add(this.chkHoleLocations);
+            this.Controls.Add(this.lblFlags);
+            this.Controls.Add(this.txtFlags);
+            this.Controls.Add(this.btnNewSeed);
+            this.Controls.Add(this.lblSeed);
+            this.Controls.Add(this.txtSeed);
+            this.Controls.Add(this.btnCompareBrowse);
+            this.Controls.Add(this.lblCompareImage);
+            this.Controls.Add(this.txtCompare);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.lblReqChecksum);
+            this.Controls.Add(this.lblRequired);
+            this.Controls.Add(this.lblSHAChecksum);
+            this.Controls.Add(this.lblSHA);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lblRomImage);
+            this.Controls.Add(this.txtFileName);
+            this.Name = "Form1";
+            this.Text = "Golf Randomizer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblFlags;
+        private System.Windows.Forms.TextBox txtFlags;
+        private System.Windows.Forms.Button btnNewSeed;
+        private System.Windows.Forms.Label lblSeed;
+        private System.Windows.Forms.TextBox txtSeed;
+        private System.Windows.Forms.Button btnCompareBrowse;
+        private System.Windows.Forms.Label lblCompareImage;
+        private System.Windows.Forms.TextBox txtCompare;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.Label lblReqChecksum;
+        private System.Windows.Forms.Label lblRequired;
+        private System.Windows.Forms.Label lblSHAChecksum;
+        private System.Windows.Forms.Label lblSHA;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblRomImage;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.CheckBox chkHoleLocations;
+        private System.Windows.Forms.CheckBox chkObstacles;
+        private System.Windows.Forms.CheckBox chkTeeBoxes;
+        private System.Windows.Forms.CheckBox chkClubs;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button cmdRandomize;
     }
 }
 
--- golfRando/Form1.cs
+++ golfRando/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,269 @@ namespace golfRando
 {
     public partial class Form1 : Form
     {
+        bool loading = true;
+        byte[] romData;
+        byte[] romData2;
+
+        int[] holeAddresses = { 0x27b4, 0x2ba9, 0x33d3, 0x35c4, 0x2aa9, 0x2fdd, 0x31e7, 0x3903, 0x37bc,
+                                0x30bf, 0x2ef9, 0x2de9, 0x28cc, 0x2991, 0x34bc, 0x2d06, 0x36b7, 0x32e2 };
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtSeed.Text = (DateTime.Now.Ticks % 2147483647).ToString();
+
+            try
+            {
+                using (TextReader reader = File.OpenText("lastgolf.txt"))
+                {
+                    txtFlags.Text = reader.ReadLine();
+                    txtFileName.Text = reader.ReadLine();
+
+                    determineChecks(null, null);
+
+                    runChecksum();
+                    loading = false;
+                }
+            }
+            catch
+            {
+                // ignore error
+                loading = false;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            using (StreamWriter writer = File.CreateText("lastgolf.txt"))
+            {
+                writer.WriteLine(txtFlags.Text);
+                writer.WriteLine(txtFileName.Text);
+            }
+        }
+
+        private void runChecksum()
+        {
+            try
+            {
+                using (var md5 = SHA1.Create())
+                {
+                    using (var stream = File.OpenRead(txtFileName.Text))
+                    {
+                        lblSHAChecksum.Text = BitConverter.ToString(md5.ComputeHash(stream)).ToLower().Replace("-", "");
+                    }
+                }
+            }
+            catch
+            {
+                lblSHAChecksum.Text = "????????????????????????????????????????";
+            }
+        }
+
+        private bool loadRom(bool extra = false)
+        {
+            try
+            {
+                romData = File.ReadAllBytes(txtFileName.Text);
+                if (extra)
+                    romData2 = File.ReadAllBytes(txtCompare.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Empty file name(s) or unable to open files.  Please verify the files exist.");
+                return false;
+            }
+            return true;
+        }
+
+        private void saveRom()
+        {
+            string options = "";
+            string finalFile = Path.Combine(Path.GetDirectoryName(txtFileName.Text), "Golf_" + txtSeed.Text + "_" + txtFlags.Text + ".nes");
+            File.WriteAllBytes(finalFile, romData);
+            lblStatus.Text = "ROM hacking complete!  (" + finalFile + ")";
+            txtCompare.Text = finalFile;
+        }
+
+        private void btnNewSeed_Click(object sender, EventArgs e)
+        {
+            txtSeed.Text = (DateTime.Now.Ticks % 2147483647).ToString();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFileName.Text = openFileDialog1.FileName;
+                runChecksum();
+            }
+        }
+
+        private void determineFlags(object sender, EventArgs e)
+        {
+            if (loading)
+                return;
+
+            string flags = "";
+            int number = (chkHoleLocations.Checked ? 1 : 0) + (chkObstacles.Checked ? 2 : 0) + (chkTeeBoxes.Checked ? 4 : 0) +
+                (chkClubs.Checked ? 8 : 0);
+            flags += convertIntToChar(number);
+
+            txtFlags.Text = flags;
+        }
+
+        private void determineChecks(object sender, EventArgs e)
+        {
+            if (txtFlags.Text.Length != 1) return;
+            loading = true;
+            string flags = txtFlags.Text;
+            int number = convertChartoInt(Convert.ToChar(flags.Substring(0, 1)));
+            chkHoleLocations.Checked = (number % 2 == 1);
+            chkObstacles.Checked = (number % 4 >= 2);
+            chkTeeBoxes.Checked = (number % 8 >= 4);
+            chkClubs.Checked = (number % 16 >= 8);
+            loading = false;
+        }
+
+        private string convertIntToChar(int number)
+        {
+            if (number >= 0 && number <= 9)
+                return number.ToString();
+            if (number >= 10 && number <= 35)
+                return Convert.ToChar(55 + number).ToString();
+            if (number >= 36 && number <= 61)
+                return Convert.ToChar(61 + number).ToString();
+            if (number == 62) return "!";
+            if (number == 63) return "@";
+            return "";
+        }
+
+        private int convertChartoInt(char character)
+        {
+            if (character >= Convert.ToChar("0") && character <= Convert.ToChar("9"))
+                return character - 48;
+            if (character >= Convert.ToChar("A") && character <= Convert.ToChar("Z"))
+                return character - 55;
+            if (character >= Convert.ToChar("a") && character <= Convert.ToChar("z"))
+                return character - 61;
+            if (character == Convert.ToChar("!")) return 62;
+            if (character == Convert.ToChar("@")) return 63;
+            return 0;
+        }
+
+        private void cmdRandomize_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                loadRom();
+                Random r1 = new Random(Convert.ToInt32(txtSeed.Text));
+                if (chkHoleLocations.Checked) randomizeHoleLocations(r1);
+                if (chkObstacles.Checked) randomizeObstacles(r1);
+                if (chkTeeBoxes.Checked) randomizeTeeBoxes(r1);
+                if (chkClubs.Checked) randomizeClubs(r1);
+                saveRom();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:  " + ex.Message);
+            }
+
+        }
+
+        private void randomizeHoleLocations(Random r1)
+        {
+            for (int i = 0; i < 18; i++)
+            {
+                // Find X/Y difference between hole and green start
+                int greenXDiff = 0;
+                int greenYDiff = 0;
+
+                greenXDiff = romData[holeAddresses[i] + 0] - romData[holeAddresses[i] + 3];
+                greenYDiff = romData[holeAddresses[i] + 1] - romData[holeAddresses[i] + 4];
+
+                bool legal = false;
+                while (!legal)
+                {
+                    romData[holeAddresses[i] + 3] = (byte)(r1.Next() % 208);
+                    romData[holeAddresses[i] + 4] = (byte)((r1.Next() % 96) + 128);
+                    if (romData[holeAddresses[i] + 3] + greenYDiff < 208 && romData[holeAddresses[i] + 3] + greenYDiff > 0 && romData[holeAddresses[i] + 4] + greenXDiff < 224 && romData[holeAddresses[i] + 4] + greenXDiff > 128)
+                    {
+                        legal = true;
+                        romData[holeAddresses[i] + 0] = (byte)(romData[holeAddresses[i] + 3] + greenXDiff);
+                        romData[holeAddresses[i] + 1] = (byte)(romData[holeAddresses[i] + 4] + greenYDiff);
+                    }
+                }
+            }
+        }
+
+        private void randomizeObstacles(Random r1)
+        {
+            for (int i = 0; i < 18; i++)
+            {
+                bool legal = false;
+                int j = 7;
+                while (!legal && romData[holeAddresses[i] + j] != 255)
+                {
+                    int startY = (byte)(r1.Next() % 208);
+                    int startX = (byte)((r1.Next() % 96) + 128);
+
+                    if (Math.Abs(startY - romData[holeAddresses[i] + 0]) >= 32 && Math.Abs(startX - romData[holeAddresses[i] + 1]) >= 32)
+                    {
+                        romData[holeAddresses[i] + j + 1] = (byte)startY;
+                        romData[holeAddresses[i] + j + 2] = (byte)startX;
+                        j += 3;
+                        legal = true;
+                    }
+                }
+            }
+        }
+
+        private void randomizeTeeBoxes(Random r1)
+        {
+            for (int i = 0; i < 18; i++)
+            {
+                bool legal = false;
+                while (!legal)
+                {
+                    int startY = (byte)(r1.Next() % 208);
+                    int startX = (byte)((r1.Next() % 96) + 128);
+
+                    if (Math.Abs(startY - romData[holeAddresses[i] + 0]) >= 32 && Math.Abs(startX - romData[holeAddresses[i] + 1]) >= 32)
+                    {
+                        romData[holeAddresses[i] + 5] = (byte)startY;
+                        romData[holeAddresses[i] + 6] = (byte)startX;
+                        legal = true;
+                    }
+                }
+            }
+        }
+
+        private void randomizeClubs(Random r1)
+        {
+            for (int i = 0; i < 13; i++)
+            {
+                romData[0x160d + i] = (byte)(inverted_power_curve(10, 250, .4, r1));
+            }
+        }
+
+        private int inverted_power_curve(int min, int max, double powToUse, Random r1)
+        {
+            int range = max - min;
+            double p_range = Math.Pow(range, 1 / powToUse);
+            double section = (double)r1.Next() / int.MaxValue;
+            int points = (int)Math.Round(max - Math.Pow(section * p_range, powToUse));
+            return points;
         }
     }
 }
